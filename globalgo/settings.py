bed0f1cc2e26c0b008ffcfd02911004392a0980f
website-krysta <website.krysta@gmail.com>
2024-08-26 17:18:01 +0530

    chenged db

--- globalgo/settings.py
+++ globalgo/settings.py
@@ -87,22 +87,22 @@ WSGI_APPLICATION = 'globalgo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#   'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'globalgo_gateway',
-#         'USER': 'globalgo_gateway_user',
-#         'PASSWORD': 'nkob47#~Q=s*',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'globalgo_gateway',
+        'USER': 'globalgo_gateway_user',
+        'PASSWORD': 'nkob47#~Q=s*',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
