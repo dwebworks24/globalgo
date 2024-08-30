77899dd29e62ca27dba1171305e92831af7e4668
website-krysta <website.krysta@gmail.com>
2024-08-30 10:31:49 +0530

    all issues fixed

--- globalgo/settings.py
+++ globalgo/settings.py
@@ -174,6 +174,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =  'krystatesting@gmail.com'
-EMAIL_HOST_PASSWORD =  'taihppzbhoeqeklu'  
+EMAIL_HOST_USER =  'noreply.globalgogateway@gmail.com'
+EMAIL_HOST_PASSWORD =  'fcoe haie blys wtak'  
 # taihppzbhoeqeklu
