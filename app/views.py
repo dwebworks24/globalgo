245a2e8acd95d2a593b5c2773302b7280e3b5056
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2025-03-06 23:34:52 +0530

    updated blog hyderabad

--- app/views.py
+++ app/views.py
@@ -16,7 +16,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 def best_consultancy_in_vijayawada(request):     
     return render(request, 'uifiles/best_consultancy_in_vijayawada.html')
-
+def best_abroad_consultancy_in_hyderabad(request):     
+    return render(request, 'uifiles/best_abroad_consultancy_in_hyderabad.html')
 def abroad_consultancy_in_andhra_pradesh(request):     
     return render(request, 'uifiles/abroad-consultancy-in-andhra-pradesh.html')
 def abroad_consultancy_in_gannavaram(request):     
